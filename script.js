d9fa2ba81bde9c51e5798c24026b5b8d2014b970
Maksym Koldun <koldun.maks@gmail.com>
2018-07-17 02:27:16 +0300

    basic chat  with  using sockets

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ var nameUser = "User name";
 userHeader.innerHTML = nameUser;
 
 var socket = io.connect();
+
+nameButton.addEventListener("click", function() {
+    nameUser = nameInput.value || "User name";
+    userHeader.innerHTML = nameUser;
+});
+
+textSubmit.addEventListener("click", function() {
+    var data = {
+        name: nameUser,
+        text: text.value
+    }
+
+    text.value = "";
+
+    socket.emit("chat message", data);
+});
+
+socket.on("chat history", function(msg) {
+    console.log(msg);
+    for(var i in msg) {
+        if(msg.hasOwnProperty(i)) {
+            var el = document.createElement("li");
+            el.innerText = `${msg[i].name}: ${msg[i].text}`;
+            messages.appendChild(el); 
+        }
+    }
+});
+
+socket.on("chat message", function(msg) {
+    var el = document.createElement("li");
+    el.innerText = `${msg.name}: ${msg.text}`;
+    messages.appendChild(el); 
+});
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var app = require("express")(),
-    http = require("http").Server(app),
-    io = require("socket.io")(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 var messages = [];
 
@@ -12,9 +12,15 @@ app.get("/script.js", function(req, res) {
     res.sendFile(__dirname + "/script.js");
 });
 
-io.get("connection", function() {
+io.on("connection", function(socket) {
     console.log("client connected");
-})
+    socket.on("chat message", function(msg) {
+        messages.push(msg);
+        io.emit("chat message", msg);
+    });
+
+    socket.emit("chat history", messages);
+});
 
 http.listen(1428, function() {
     console.log("litening on *:1428");
