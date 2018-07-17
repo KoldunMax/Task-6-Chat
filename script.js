d8262dd686ae1419eea9c0e2f7fae12d57a1bd3a
Maksym Koldun <koldun.maks@gmail.com>
2018-07-17 17:32:40 +0300

    adding user board

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var nameInput = document.getElementById("nameInput");
 var messages = document.getElementById("messages");
 var text = document.getElementById("text");
 var textSubmit = document.getElementById("textSubmit");
+// -======================================================--==----------========
+var inviteButton = document.getElementById("inviteButton");
+var inviteName = document.getElementById("inviteName");
+var inviteNick = document.getElementById("inviteNick");
+var ULasideUsers = document.getElementById("users-name-list");
+
+console.log(ULasideUsers);
 
 var nameUser = "User name";
 userHeader.innerHTML = nameUser;
@@ -14,6 +21,15 @@ nameButton.addEventListener("click", function() {
     nameUser = nameInput.value || "User name";
     userHeader.innerHTML = nameUser;
 });
+
+inviteButton.addEventListener("click", function() {
+    var newUser = {
+        name: inviteName.value,
+        nickname: inviteNick.value
+    }
+
+    socket.emit("chat user", newUser);
+})
 
 textSubmit.addEventListener("click", function() {
     var data = {
@@ -41,4 +57,83 @@ socket.on("chat message", function(msg) {
     var el = document.createElement("li");
     el.innerText = `${msg.name}: ${msg.text}`;
     messages.appendChild(el); 
+});
+
+socket.on("adding user", function(users) {
+
+    for(var i in users) {
+        if (users.hasOwnProperty(i)) {
+
+            var el = document.createElement("li");
+            var onlineC = document.createElement("span");
+            var onlinelabel = document.createElement("span");
+            var nameOfUser = document.createElement("span");
+            onlinelabel.className = "state-time-coming";
+            nameOfUser.innerText = `${users[i].nickname}`;
+
+            if(users[i].status == "online") {
+                onlineC.className = "online";
+                onlinelabel.innerText = "online";
+            }
+        
+            if(users[i].status == "just appeared") {
+                onlineC.className = "online";
+                onlinelabel.innerText = "just appeared";
+            }
+        
+            if(users[i].status == "offline") {
+                onlineC.className = "offline";
+                onlinelabel.innerText = "offline";
+            }
+        
+            if(users[i].status == "just left") {
+                onlineC.className = "offline";
+                onlinelabel.innerText = "just left";
+            }
+
+            el.appendChild(onlinelabel);
+            el.insertBefore(nameOfUser, onlinelabel);
+            el.insertBefore(onlineC, nameOfUser);
+
+            ULasideUsers.appendChild(el); 
+        }
+    }
+});
+
+socket.on("change status", function(user) {
+    for(let obj of ULasideUsers.getElementsByTagName("li")) {
+        if(obj.children[1].innerText == user.nickname) {
+            obj.children[2].innerText = user.status;
+            if(user.status == "offline" || user.status == "just left"){
+                obj.children[0].className = "offline";
+            } else {
+                obj.children[0].className = "online";
+            }
+        }
+    }
+})
+
+/* 
+for(let obj of ULasideUsers.getElementsByTagName("li")) {
+	obj.children[0].className = "offline";
+	obj.children[1].innerText = "just left"
+}
+*/
+
+socket.on("chat user", function(user) {
+    var el = document.createElement("li");
+    var onlineC = document.createElement("span");
+    var onlinelabel = document.createElement("span");
+    var nameOfUser = document.createElement("span");
+    nameOfUser.innerText = `${user.nickname}`;
+    onlinelabel.className = "state-time-coming";
+
+    onlineC.className = "online";
+    onlinelabel.innerText = "just appeared";
+
+    el.appendChild(onlinelabel);
+    el.insertBefore(nameOfUser, onlinelabel);
+    el.insertBefore(onlineC, nameOfUser);
+    console.log(el);
+    ULasideUsers.appendChild(el); 
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 var messages = [];
+var users = [];
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -17,13 +18,34 @@ app.get("/style.css", function(req, res) {
 });
 
 io.on("connection", function(socket) {
+
     console.log("client connected");
+
     socket.on("chat message", function(msg) {
         messages.push(msg);
         io.emit("chat message", msg);
     });
 
+    socket.on("chat user", function(newUser) {
+        newUser.dataInv = new Date();
+        newUser.status = "just appeared";
+        newUser.id = socket.id;
+        users.push(newUser);
+        io.emit("chat user", newUser);
+    })
+
+    socket.emit("adding user",  users);
     socket.emit("chat history", messages);
+
+    socket.on('disconnect', function(){
+        for(let i = 0; i < users.length; i++) {
+            if(users[i].id == socket.id) {
+                console.log(`${users[i].nickname} is disconnected`);
+                users[i].status = "just left"; 
+                io.emit("change status",  users[i]);
+            }
+        }
+    });
 });
 
 http.listen(1428, function() {
